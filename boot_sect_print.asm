3c9be37cb7a60e815981582fe794a7fb9123565e
Sophia Feng <sophiafengpku@gmail.com>
2014-10-16 22:32:33 -0700

    add a boot_sect that prints a message

---
+++ boot_sect_print.asm
@@ -0,0 +1,30 @@
+; a boot sector that prints a message to screen with BIOS's interrupt
+
+mov ah, 0x0e
+
+mov al, 'P'
+int 0x10
+mov al, 'H'
+int 0x10
+mov al, 'I'
+int 0x10
+mov al, 'L'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'P'
+int 0x10
+mov al, 'H'
+int 0x10
+mov al, 'I'
+int 0x10
+mov al, 'A'
+int 0x10
+
+jmp $ ; jump to the current address
+times 510-($-$$) db0 ; pad 510 zeroes in the boot sector
+dw 0xaa55 ; magic number at last two bytes
